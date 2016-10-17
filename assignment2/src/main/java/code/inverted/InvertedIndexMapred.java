32887f6544f9c5c3e033b20d53f8aa6a9bf6adaa
Jessica Huynh <jessicahhuynh@outlook.com>
2016-10-17 11:38:46 -0400

    configure map and reduce jobs

--- assignment2/src/main/java/code/inverted/InvertedIndexMapred.java
+++ assignment2/src/main/java/code/inverted/InvertedIndexMapred.java
@@ -2,10 +2,18 @@ package code.inverted;
 
 import java.io.IOException;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
 
+import code.articles.GetArticlesMapred;
+import code.articles.GetArticlesMapred.GetArticlesMapper;
 import util.StringIntegerList;
 import util.StringIntegerList.StringInteger;
 
@@ -34,8 +42,26 @@ public class InvertedIndexMapred {
 		}
 	}
 
-	public static void main(String[] args) {
-		// TODO: you should implement the Job Configuration and Job call
-		// here
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		String[] otherArgs = new GenericOptionsParser(conf,args).getRemainingArgs();
+		if (otherArgs.length != 2) {
+			System.err.println("Incorrect arguments");
+			System.exit(2);
+		}
+		
+		Job job = new Job(conf,"invert index");
+		job.setJarByClass(InvertedIndexMapred.class);
+		job.setMapperClass(InvertedIndexMapper.class);
+		job.setCombinerClass(InvertedIndexReducer.class);
+		job.setReducerClass(InvertedIndexReducer.class);
+		
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(StringInteger.class);
+		
+		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+		
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
 }
