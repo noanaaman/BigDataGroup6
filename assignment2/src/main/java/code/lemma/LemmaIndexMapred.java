ae0d88a22d925060ade7ebbfe8d602f4721ea8eb
clayriley <riley@brandeis.edu>
2016-10-17 15:58:16 -0400

    wrote the non-lemmatization parts of Lemma mapper

--- assignment2/src/main/java/code/lemma/LemmaIndexMapred.java
+++ assignment2/src/main/java/code/lemma/LemmaIndexMapred.java
@@ -9,6 +9,13 @@ import org.apache.hadoop.mapreduce.Mapper;
 import util.StringIntegerList;
 import edu.umd.cloud9.collection.wikipedia.WikipediaPage;
 
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.ArrayList;
+import util.StringIntegerList;
+import util.StringIntegerList.StringInteger;
+
 /**
  * 
  *
@@ -25,8 +32,33 @@ public class LemmaIndexMapred {
 			//    -- page.getContent() --> String of the text content of the page
 			// 2. tokenize it using the Tokenizer class
 			//    -- Tokenizer.tokenize(string) --> list of strings of tokens
-			// 4. drop the stopwords and lemmatize
+			// 4. lemmatize
 			// 5. output: Text Title, StringIntegerList lemmas_and_counts
+			//    COMPLETE
+			
+			List<String> lemmas = Tokenizer.lemmatize(page);
+			Text title = page.getTitle();
+			Map<String, Integer> frequencies = new HashMap<String,Integer>();
+			// loop through all lemmas for this page
+			for (String lemma : lemmas) {
+				Integer f = frequencies.get(lemma);
+				// check for null
+				if (f == null) {
+					f = new Integer(0);
+				}
+				// update or create the frequency for this lemma
+				frequencies.put(lemma, f+1);
+			}
+			// convert the frequency map to a StringIntegerList ...
+			List<StringInteger> lemmacounts = new ArrayList<StringInteger>();
+			for (Map.Entry<String,Integer> e : frequencies.entrySet()) {
+				StringInteger lemmacount = new StringInteger(e.getKey(), e.getValue());
+				lemmacounts.add(lemmacount);
+			}
+			// ... and write it to the output
+			context.write(title, new StringIntegerList(lemmacounts));
 		}
 	}
+	
+	// no Reducer is needed.
 }
