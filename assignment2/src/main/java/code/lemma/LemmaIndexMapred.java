5bb90cc2c4c2771e1f3c10dd86fad91302c4d3f6
clayriley <riley@brandeis.edu>
2016-10-17 14:38:19 -0400

    Added comments to Lemma Index MR

--- assignment2/src/main/java/code/lemma/LemmaIndexMapred.java
+++ assignment2/src/main/java/code/lemma/LemmaIndexMapred.java
@@ -19,6 +19,12 @@ public class LemmaIndexMapred {
 		@Override
 		public void map(LongWritable offset, WikipediaPage page, Context context) throws IOException, InterruptedException {
 			// TODO: implement Lemma Index mapper here
+			
+			// TODO:
+			// 1. get the content of the input wikipedia page
+			//    -- page.getContent() --> String of the text content of the page
+			// 2. tokenize it using the Tokenizer class
+			//    -- Tokenizer.tokenize(string) --> list of strings of tokens
 		}
 	}
 }
