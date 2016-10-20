9da45048fa1675a0281632379a60786c411f572f
Jessica Huynh <jessicahhuynh@outlook.com>
2016-10-20 12:30:54 -0400

    add main for lemma indexer

--- assignment2/src/main/java/code/lemma/LemmaIndexMapred.java
+++ assignment2/src/main/java/code/lemma/LemmaIndexMapred.java
@@ -2,10 +2,19 @@ package code.lemma;
 
 import java.io.IOException;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
 
+import code.inverted.InvertedIndexMapred;
+import code.inverted.InvertedIndexMapred.InvertedIndexMapper;
+import code.inverted.InvertedIndexMapred.InvertedIndexReducer;
 import util.StringIntegerList;
 import edu.umd.cloud9.collection.wikipedia.WikipediaPage;
 
@@ -60,4 +69,25 @@ public class LemmaIndexMapred {
 	}
 	
 	// no Reducer is needed.
+	
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		String[] otherArgs = new GenericOptionsParser(conf,args).getRemainingArgs();
+		if (otherArgs.length != 2) {
+			System.err.println("Incorrect arguments");
+			System.exit(2);
+		}
+		
+		Job job = Job.getInstance(conf);
+		job.setJarByClass(LemmaIndexMapred.class);
+		job.setMapperClass(LemmaIndexMapper.class);
+		
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(StringIntegerList.class);
+		
+		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+		
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
 }
