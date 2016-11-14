af7659f5e86e348c7f24b779fa2aff2863b8225c
clayriley <riley@brandeis.edu>
2016-11-14 10:59:12 -0500

    Corrected preprocessing shell script; added test file for demonstration

--- assignment3/preproc.sh
+++ assignment3/preproc.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# grep -m 1
-
-while read -r line || [[ -n "$line" ]]; do
+while read -r line || [ -n "$line" ]; do
   # complete some tasks with $line
   # get the article title for matching against the professions file
-  title = sed -e 's/\(.*\)<.*/\1/' -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' $line
+  title=$(echo $line | sed -e 's/\([^<]*\)<.*/\1/' -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
 
   ## match title against professions file
   #profs = grep -m 1 $title professions.txt
@@ -16,24 +14,40 @@ while read -r line || [[ -n "$line" ]]; do
   #  continue;
   #fi
 
-  # store the lemmas and indices too
-  wcs = sed -e 's/.*\(.*\)/\1/' -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' $line
+  # store the lemmas and indices: grab everything after the title
+  wcs=$(echo $line | sed -e 's/[^<]*\(.*\)/\1/' -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
   # clear out sequence variable for current instance
-  seq = ""
-  # continue until wcs is emptied
+  seq=""
+
+  # continue until no more lemma counts remain
   while [ -n "$wcs" ]; do
-    # current lemma and count
-    lemma = sed -e 's/<\(.*\),.*/\1/' $wcs
-    count = sed -e 's/.*,\([0-9]*\)>/\1/' $wcs
-    # update wcs
-    wcs = sed -e 's/.*>,\(.*\)/\1/' $wcs 
-    # repeat the lemma x count
-    for i in { 1..$count } do
+
+    # current lemma and count:
+    # lemma: everything between open bracket and comma
+    lemma=$(echo $wcs | sed -e 's/< *\([^,]*\).*/\1/')
+    # count: digits between comma and close bracket
+    count=$(echo $wcs | sed -e 's/[^,]*,\([0-9]*\)>.*/\1/')
+    # update wcs: cut off everything before first 
+    wcs=$(echo $wcs | sed -e 's/[^>]*>,*\(.*\)/\1/')
+
+    #echo $lemma
+    #echo $count
+    #echo
+
+    # repeat the lemma count times in the current title's sequence
+    for i in $(seq 1 $count); do
       # add this series of strings to the current sequence
       seq=$seq$lemma" "  
+      #echo "repeating $lemma $count times; currently at $i"
+    done
+    #echo $seq
   done
+  #echo $seq
   
   # write the title and sequence to file
-  echo -e "$title\t$seq" >> sequences.txt
+  echo $title"\t"$seq >> sequences.txt
 
-done
+##############################################################
+# ADJUST THIS PATH TO CHANGE SOURCE OF ARTICLE-LEMMA INDICES #
+##############################################################
+done <test.txt
